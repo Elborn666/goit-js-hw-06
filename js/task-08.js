88fc80ee1aefc4906f9a8423c437663a68405852
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-03-16 15:34:07 +0200

    done 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const emailRef = document.querySelector('[name="email"]')
+const paswRef = document.querySelector('[name="password"]')
+const btnRef = document.querySelector('button')
+
+const formRef = document.querySelector('.login-form');
+
+formRef.addEventListener("submit", handleSubmit);
+
+const elem = {}
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+const emailRef = event.currentTarget.elements.email;
+const paswRef = event.currentTarget.elements.password;
+
+if (emailRef.value === '' || paswRef.value === ''){
+    return alert('Заповніть всі поля')
+} else {
+    elem.emailRef = emailRef.value
+    elem.paswRef = paswRef.value
+    console.log(elem);
+}
+event.currentTarget.reset();
+}
+
 
